f40638783435d78d0cca4ee9afb410032990844c
Pedro Henrique <ph29@outlook.com.br>
2025-12-22 10:45:25 -0300

    remove imports

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/pages/home.html')
