2121c2815b01c0c7f781b0e31b1874b224b471d2
Pedro Paiva <ph29@outlook.com.br>
2026-01-03 16:01:11 -0300

    add context in views

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import request_recipes
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [request_recipes() for _ in range(9)],
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': request_recipes(),
+    })
